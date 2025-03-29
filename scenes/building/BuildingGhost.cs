1e29a3da5463554539ffbbaf694c67e5fe96e8d7
len <stanislaw.len@proton.me>
2025-03-29 23:30:55 +0100

    feat: add region corners to building ghost indicators

--- scenes/building/BuildingGhost.cs
+++ scenes/building/BuildingGhost.cs
@@ -4,7 +4,18 @@ namespace Game.Building;
 
 public partial class BuildingGhost : Node2D
 {
-    public override void _Ready() { }
+    private Node2D topLeft;
+    private Node2D topRight;
+    private Node2D bottomLeft;
+    private Node2D bottomRight;
+
+    public override void _Ready()
+    {
+        topLeft = GetNode<Node2D>("TopLeft");
+        topRight = GetNode<Node2D>("TopRight");
+        bottomLeft = GetNode<Node2D>("BottomLeft");
+        bottomRight = GetNode<Node2D>("BottomRight");
+    }
 
     public void SetInvalid()
     {
@@ -14,5 +25,12 @@ public partial class BuildingGhost : Node2D
     public void SetValid()
     {
         Modulate = Colors.White;
+    }
+
+    public void SetDimensions(Vector2I dimentions)
+    {
+        topRight.Position = dimentions * new Vector2I(64, 0);
+        bottomLeft.Position = dimentions * new Vector2I(0, 64);
+        bottomRight.Position = dimentions * new Vector2I(64, 64);
     }
 }
